c718a292c4a5b036f9fccbb7471b9c049f11539c
pandita64 <163213265+pandita64@users.noreply.github.com>
2024-09-25 00:28:49 -0500

    consumo de datos

--- src/componentes/UserPage.tsx
+++ src/componentes/UserPage.tsx
@@ -1,27 +1,47 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { RickAndMortyAPIResponse, Character } from "../interfaces";
 
+const loadCharacters = async (): Promise<Character[]> => {
+    try {
+        const { data } = await axios.get<RickAndMortyAPIResponse>('https://rickandmortyapi.com/api/character');
+        return data.results;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+};
 
 const UserPage = () => {
+    const [characters, setCharacters] = useState<Character[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const fetchedCharacters = await loadCharacters();
+            setCharacters(fetchedCharacters);
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <>
-        <h3>Personajes de Ricky Morty</h3>
-        <table>
-            <thead>
-                <tr>
-                    <th>Personaje</th>
-                    <th>Ubicacion</th>
-                    <th>Episodio</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>personaje</td>
-                    <td>ubicacion</td>
-                    <td>episodio</td>
-                </tr>
-            </tbody>
-        </table>
+            <h3>Personajes de Rick y Morty</h3>
+            <div className="card-container">
+                {characters.map(character => (
+                    <div key={character.id} className="card">
+                        <img src={character.image} alt={character.name} className="image" />
+                        <h4>{character.name}</h4>
+                        <p><strong>Ubicación:</strong> {character.location.name}</p>
+                        <p><strong>Número de Episodios:</strong> {character.episode.length}</p>
+                    </div>
+                ))}
+            </div>
         </>
-    )
-   
-}
-export default UserPage
+    );
+};
+
+export default UserPage;
+
+
+
